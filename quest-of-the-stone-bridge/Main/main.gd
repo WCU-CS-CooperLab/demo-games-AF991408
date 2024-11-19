cb1b18d3b9ca26ca3690e21c8df0e43cdfa94011
AF991408 <112415680+AF991408@users.noreply.github.com>
2024-11-19 13:12:04 -0500

    fixed

--- quest-of-the-stone-bridge/Main/main.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-func _ready():
-	var level_num = str(GameState.current_level).pad_zeros(2)
-	var path = "res://Levels/level_%s.tscn" % level_num
-	var level = load(path).instantiate()
-	add_child(level)
--- quest-of-the-stone-bridge/global_data.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-var player_time: float = 0.0
-
-var save_data:SaveData
-
-func _ready():
-	save_data = SaveData.load_or_create()
